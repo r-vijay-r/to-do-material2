9065249805675a2e0221eab95fa682a2319c647d
r-vijay-r <503r.vijay@gmail.com>
2016-10-29 00:14:01 +0530

    todos ordered by reverse order using maping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
-
+import "rxjs/add/operator/map";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,38 +16,39 @@ export class AppComponent {
   editItem:string;
   todos:FirebaseListObservable<any[]>;
   constructor(public af:AngularFire){
-  	this.intervalUID=setInterval(()=>{
-  		this.uid=document.getElementById("uid");
-  		if(this.uid.value!="not_set"){
-  			this.todos=this.af.database.list('/'+this.uid.value);
-  			this.active=true;
-  			this.urlset=true;
-  			clearInterval(this.intervalUID);
-  		}
-  	},1000);
+    this.intervalUID=setInterval(()=>{
+      this.uid=document.getElementById("uid");
+      if(this.uid.value!="not_set"){
+        this.todos=this.af.database.list('/'+this.uid.value).map((array) => array.reverse()) as FirebaseListObservable<any[]>;;
+        //this.todos=this.todos;
+        this.active=true;
+        this.urlset=true;
+        clearInterval(this.intervalUID);
+      }
+    },1000);
   }
   submit( todo:string){
-  	this.todos.push({"todo":todo,"status":"incomplete"});
-  	this.active=false;
-  	setTimeout(()=>{this.active=true;},0);
+    this.todos.push({"todo":todo,"status":"incomplete"});
+    this.active=false;
+    setTimeout(()=>{this.active=true;},0);
   }
   statusUpdater(key:string, status:string){
-  	if(status=="incomplete"){
-  		this.todos.update(key,{"status":"completed"});
-  	}else if(status=="completed"){
-	  	this.todos.update(key,{"status":"incomplete"});
-  	}
+    if(status=="incomplete"){
+      this.todos.update(key,{"status":"completed"});
+    }else if(status=="completed"){
+      this.todos.update(key,{"status":"incomplete"});
+    }
   }
   deleteItem(key:string){
-  	this.todos.remove(key);
+    this.todos.remove(key);
   }
   edit(key:string, value:string){
-  	this.editItemKey=key;
-  	this.editItem=value;
-  	this.active=false;
+    this.editItemKey=key;
+    this.editItem=value;
+    this.active=false;
   }
   update(value:string){
-  	this.todos.update(this.editItemKey,{"todo":value});  	
-  	setTimeout(()=>{this.active=true;},0);
+    this.todos.update(this.editItemKey,{"todo":value});    
+    setTimeout(()=>{this.active=true;},0);
   }
 }
