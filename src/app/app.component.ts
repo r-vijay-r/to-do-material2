95333dd1ab584bfbf77a0a42c7ba07f398d672a6
r-vijay-r <503r.vijay@gmail.com>
2016-10-28 17:58:17 +0530

    todo add working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'To-Do App Material2!';
+  active:boolean=false;
+  urlset:boolean=false;
+  intervalUID;
+  uid;
+  editItemKey:string;
+  editItem:string;
+  todos:FirebaseListObservable<any[]>;
+  constructor(public af:AngularFire){
+  	this.intervalUID=setInterval(()=>{
+  		this.uid=document.getElementById("uid");
+  		if(this.uid.value!="not_set"){
+  			this.todos=this.af.database.list('/'+this.uid.value);
+  			this.active=true;
+  			this.urlset=true;
+  			clearInterval(this.intervalUID);
+  		}
+  	},1000);
+  }
+  submit( todo:string){
+  	this.todos.push({"todo":todo,"status":"incomplete"});
+  	this.active=false;
+  	setTimeout(()=>{this.active=true;},0);
+  }
+  statusUpdater(key:string, status:string){
+  	if(status=="incomplete"){
+  		this.todos.update(key,{"status":"completed"});
+  	}else if(status=="completed"){
+	  	this.todos.update(key,{"status":"incomplete"});
+  	}
+  }
+  deleteItem(key:string){
+  	this.todos.remove(key);
+  }
+  edit(key:string, value:string){
+  	this.editItemKey=key;
+  	this.editItem=value;
+  	this.active=false;
+  }
+  update(value:string){
+  	this.todos.update(this.editItemKey,{"todo":value});  	
+  	setTimeout(()=>{this.active=true;},0);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import {AngularFireModule, AuthProviders, AuthMethods} from 'angularfire2';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHeN-Y9QA6dKHP9mYPF93yHXFDL-4o4nU",
+  authDomain: "to-do-mat2.firebaseapp.com",
+  databaseURL: "https://to-do-mat2.firebaseio.com/",
+  storageBucket: "to-do-mat2.appspot.com"
+}
+const AuthenticationConfig = {
+  method: AuthMethods.Popup,
+  provider: AuthProviders.Google
+ }
 
 @NgModule({
   declarations: [
@@ -12,7 +25,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig, AuthenticationConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
